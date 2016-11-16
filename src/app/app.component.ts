4ffb6576b0b763f9aa388dd6c388d3370a3d737b
James West <jameswest@Jamess-Air.home>
2016-11-16 18:51:31 +0000

    test removal swipeBackEnabled false

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  template: `<ion-nav [root]="rootPage" swipeBackEnabled="false"></ion-nav>`
 })
 export class MyApp {
   rootPage = TabsPage;
@@ -17,6 +17,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
     });
   }
 }
